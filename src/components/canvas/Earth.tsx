b22dc70a1c5310a195b5af7ed51003fa1f9cce95
nitesh8080 <officialnitesh14@gmail.com>
2026-05-28 16:41:13 +0530

    fix mobile laptop 3d model

--- src/components/canvas/Earth.tsx
+++ src/components/canvas/Earth.tsx
@@ -1,24 +1,46 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../layout/Loader";
 
-const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+const Earth = ({ isMobile }: { isMobile: boolean }) => {
+  const earth = useGLTF("/planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive
+      object={earth.scene}
+      scale={isMobile ? 1.4 : 2.5}
+      position-y={0}
+      rotation-y={0}
+    />
   );
 };
 
 const EarthCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-      shadows
+      shadows={false}
       frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
+      dpr={[1, 1.5]}
+      gl={{ preserveDrawingBuffer: true, antialias: false }}
       camera={{
         fov: 45,
         near: 0.1,
@@ -34,8 +56,7 @@ const EarthCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Earth />
-
+        <Earth isMobile={isMobile} />
         <Preload all />
       </Suspense>
     </Canvas>
